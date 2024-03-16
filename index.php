b6e77dd3196bf9cf2a48a12a4a432f3421718703
Andreas-Huning <148219563+Andreas-Huning@users.noreply.github.com>
2024-03-16 11:57:51 +0100

    Tasks erstellt und erster versuch Datei auszulesen

--- index.php
+++ index.php
@@ -1,6 +1,54 @@
 <?php
 #**********************************************************************************#
 
+				#***************************#
+                #********** Tasks **********#
+                #***************************#
+/*
+				[] Welche 10 Lizenzen greifen am häufigsten zu und wie oft?
+				[] Lizenzverstöße - Eine Lizenz auf einem Gerät erlaubt?
+				[] Welche Lizenz auf welcher Hardware
+*/
+
+#**********************************************************************************#
+
+				#**************************#
+                #********** ToDo **********#
+                #**************************#
+/*
+				[x] Datei auslesen - Date zu groß und kann nicht ausgelesen werden
+				[] Datei Zeilenweise auslesen
+
+*/
+#**********************************************************************************#
+
+				#*******************************#
+                #********** DEBUGGING **********#
+                #*******************************#
+
+				define('DEBUG', 	false);	// Debugging for main document
+                define('DEBUG_V', 	false);	// Debugging for values
+                define('DEBUG_F', 	false);	// Debugging for functions
+
+#**********************************************************************************#
+
+                // Dateipfad zur Logdatei
+                $logDatei = './updatev12-access-pseudonymized.log';
+
+#**********************************************************************************#
+
+				#************************************#
+                #********** Datei auslesen **********#
+                #************************************#
+
+				// Date zu groß und kann nicht ausgelesen werden
+// 				$fileContentArray = file($logDatei);				
+// if(DEBUG_V)		echo "<pre class='debug value'><b>Line " . __LINE__ . "</b>: \$arrayName <i>(" . basename(__FILE__) . ")</i>:<br>\n";					
+// if(DEBUG_V)		print_r($array);					
+// if(DEBUG_V)		echo "</pre>";
+
+
+
 #**********************************************************************************#
 
 ?>
