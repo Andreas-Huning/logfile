3dc736a02861d02a7d8bb587e3615729b81e419e
Andreas-Huning <148219563+Andreas-Huning@users.noreply.github.com>
2024-03-16 11:33:36 +0100

    Index und css Datei angelegt

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+#**********************************************************************************#
+
+#**********************************************************************************#
+
+?>
+
+<!doctype html>
+
+<html>
+	
+	<head>	
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="./debug.css">		
+		<title>Logdatei</title>
+	</head>
+	
+	<body>	
+		<h1>Logdatei auslesen</h1>
+
+	</body>
+	
+</html>
