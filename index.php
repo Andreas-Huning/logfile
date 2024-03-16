a3166b21c507742679d1946ac5e3f0b994dc83eb
Andreas-Huning <148219563+Andreas-Huning@users.noreply.github.com>
2024-03-16 12:36:06 +0100

    Eine Zeile auslesen

--- index.php
+++ index.php
@@ -26,14 +26,25 @@
                 #********** DEBUGGING **********#
                 #*******************************#
 
-				define('DEBUG', 	false);	// Debugging for main document
-                define('DEBUG_V', 	false);	// Debugging for values
-                define('DEBUG_F', 	false);	// Debugging for functions
+				define('DEBUG', 	true);	// Debugging for main document
+                define('DEBUG_V', 	true);	// Debugging for values
+                define('DEBUG_F', 	true);	// Debugging for functions
 
 #**********************************************************************************#
 
-                // Dateipfad zur Logdatei
+				#***************************************#
+                #********** Pfad zur Logdatei **********#
+                #***************************************#
+
                 $logDatei = './updatev12-access-pseudonymized.log';
+
+#**********************************************************************************#
+
+				#*******************************#
+                #********** Variablen **********#
+                #*******************************#
+
+				$zeilenNummer = 5;
 
 #**********************************************************************************#
 
@@ -48,6 +59,67 @@
 // if(DEBUG_V)		echo "</pre>";
 
 
+#**********************************************************************************#
+
+				#*****************************************#
+                #********** READ DATA FROM FILE **********#
+                #*****************************************#
+
+				// Eine Zeile der Datei auslesen
+				function readDatafromFile($datei,$zeilenNummer)
+				{
+if(DEBUG_F)			echo "<p class='debug function'>🌀 <b>Line " . __LINE__ . "</b>: Aufruf " . __FUNCTION__ . "() <i>(" . basename(__FILE__) . ")</i></p>\n";
+
+					// Datei im Lesemodus öffnen
+					$handle = fopen($datei, "r");
+
+					// Prüfen ob Datei geöffnet werden konnte
+					if ($handle)
+					{
+if(DEBUG)				echo "<p class='debug ok'><b>Line " . __LINE__ . "</b>: LogDatei erfolgreich geöffnet <i>(" . basename(__FILE__) . ")</i></p>\n";				
+
+						$aktuelleZeile = 1;
+
+						// Jede Zeile lesen bis das Ende der Datei erreicht wird
+						while(($zeile = fgets($handle)) !== false)
+						{
+							// Zeile gefunden 
+							if($aktuelleZeile === $zeilenNummer)
+							{
+if(DEBUG)						echo "<p class='debug ok'><b>Line " . __LINE__ . "</b>: Gesuchte Zeile wurde gefunden <i>(" . basename(__FILE__) . ")</i></p>\n";				
+
+if(DEBUG_V)						echo "<p class='debug value'><b>Line " . __LINE__ . "</b>: \$zeile: $zeile <i>(" . basename(__FILE__) . ")</i></p>\n";
+								
+								// Den Abschnitt mit der Seriennumer suchen
+								preg_match('/serial=(\S+)/', $zeile, $matches);
+								$serial = $matches[1]; // Serial extrahieren
+if(DEBUG_V)						echo "<p class='debug value'><b>Line " . __LINE__ . "</b>: \$serial: $serial <i>(" . basename(__FILE__) . ")</i></p>\n";
+
+
+								// Datei schließen
+								fclose($handle);
+if(DEBUG)						echo "<p class='debug hint'><b>Line " . __LINE__ . "</b>: LogDatei geschlossen <i>(" . basename(__FILE__) . ")</i></p>\n";				
+								return;
+
+							}// Zeile gefunden END
+
+								// Zeile hochzählen
+								$aktuelleZeile++;
+if(DEBUG)						echo "<p class='debug'><b>Line " . __LINE__ . "</b>: Zeile in der Logdatei: \$aktuelleZeile: $aktuelleZeile   <i>(" . basename(__FILE__) . ")</i></p>\n";
+
+						}// Jede Zeile lesen bis das Ende der Datei erreicht wird END
+
+
+					}else
+					{
+if(DEBUG)				echo "<p class='debug err'><b>Line " . __LINE__ . "</b>: FEHLER: LogDatei konnte nicht geöffnet werden <i>(" . basename(__FILE__) . ")</i></p>\n";				
+
+					}// Prüfen ob Datei geöffnet werden konnte END
+
+
+				}// Eine Zeile der Datei auslesen END
+
+readDatafromFile($logDatei, $zeilenNummer);
 
 #**********************************************************************************#
 
